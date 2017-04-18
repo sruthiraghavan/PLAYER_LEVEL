bcb23bfbbafca59c0d2bb8b94322ea54ea5af4ea
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-04-18 19:10:35 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,1 +1,10 @@
-# PLAYER_LEVEL
+class reverse
+{
+  public static void main(String args[])
+  {
+    StringBuffer sb=new StringBuffer(args[0])
+      sb.reverse();
+      System.out.print(sb);
+  }
+}
+
