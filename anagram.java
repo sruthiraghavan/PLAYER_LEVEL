2d518a56036b94ddccb7c7454a9eeb2abf3fc4d0
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-04-20 22:15:01 +0530

    Update anagram.java

--- anagram.java
+++ anagram.java
@@ -1,1 +1,37 @@
-
+import java.util.*;
+import java.io.*;
+class anagram
+{
+    public static void main(String args[])
+    {
+        String s=args[0].toLowerCase();
+        String s1=args[0].toLowerCase();
+        char[] a=s.toCharArray();
+        char[] b=s1.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int x=0;
+        if(a.length==b.length)
+        {
+            for(int i=0;i<a.length;i++)
+            {
+                if(a[i]==b[i])
+                {
+                    x++;
+                }
+            }
+            if(x==a.length)
+            {
+                System.out.println("yes");
+            }
+            else
+            {
+                System.out.println("no");
+            }
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
